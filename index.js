eb5b842dc43825e48ba8f5512151f087fd050672
Camilo Canclini <camilocanclini@gmail.com>
2023-03-15 17:05:26 -0300

    Concretando Diseño de Index | Login Diseñado

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 let ejs = require('ejs');
 const path = require('path');
+const bcryptjs = require('bcryptjs');
 
 // Vars Configuration
 const app = express()
@@ -12,9 +13,17 @@ const portServer = process.env.SERVER_PORT_DEV;
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+// Routes Configuration
+app.use('/',require('./src/routes/userRouter.js'))
 
 app.get('/',(req,res)=>{
     res.render('index.ejs')
+})
+app.get('/login',(req,res)=>{
+    res.render('login.ejs')
 })
 
 app.listen(portServer,()=>{
